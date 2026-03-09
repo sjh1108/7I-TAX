0cd1f0572d51d83e9c67af167b8fb140268bef8f
sjh1108 <ch6854@naver.com>
2026-03-09 10:04:29 +0900

    INIT

---
+++ BE/src/main/java/com/ssafy/tax7i/Tax7iApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tax7i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tax7iApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tax7iApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssafy/tax7i/Tax7iApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tax7i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tax7iApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
